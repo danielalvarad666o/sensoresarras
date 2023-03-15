ee6143dcec841dedcb6302a51f4ccec309b9c186
jesusdanielgascaalvrado@gmail.com <jesusdanielgascaalvrado@gmail.com>
2023-03-14 22:54:06 -0600

    lectuta

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from Clases import claseUltrasonico
+from Clases import claseUltrasonico,claseTemperatura
 
 
 
-sensor = claseUltrasonico.SensorUltrasónico(trigger_pin=18, echo_pin=24)
-distancia = sensor.medir_distancia()
-print(f"Distancia: {distancia} cm")
+while True:
+ sensor_temperatura = claseTemperatura.SensorTemperatura(sensor_id='28-000000000000')
+ sensor = claseUltrasonico.SensorUltrasónico(trigger_pin=18, echo_pin=24)
+ distancia = sensor.medir_distancia()
+ temperatura = sensor_temperatura.leer_temperatura()
+
+# Imprimir resultados
+ if distancia:
+    print(f"Distancia: {distancia} cm")
+ else:
+    print("No se pudo medir la distancia.")
+
+ if temperatura:
+    print(f"Temperatura: {temperatura} °C")
+ else:
+    print("No se pudo leer la temperatura.")
